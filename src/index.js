c35b4fc0009beb5f0a401a2f828c0334fd60a86d
AliTaee <ali.taee@zoodfood.com>
2021-06-30 17:54:05 +0430

    Add webpack config for image files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import './style.css'
+import webpackIcon from './webpack-icon.png'
 
 function component() {
     const element = document.createElement('div');
@@ -7,6 +8,11 @@ function component() {
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('header')
+
+    const icon = new Image()
+    icon.src = webpackIcon
+    icon.alt = 'webpack logo'
+    element.appendChild(icon)
   
     return element;
   }
