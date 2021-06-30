1c007b893d050068f367191277be16790fc9767a
AliTaee <ali.taee@zoodfood.com>
2021-06-30 17:24:37 +0430

    Add css to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,16 @@ module.exports = {
     mode: 'production',
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ]
     }
 }
 
