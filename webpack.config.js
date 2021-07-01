776904d984c947ba4669f46ec095f74a03893449
AliTaee <ali.taee.72@gmail.com>
2021-07-01 11:12:05 +0430

    Add HtmlWebpackPlugin to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "production",
@@ -10,6 +11,11 @@ module.exports = {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack",
+    }),
+  ],
   module: {
     rules: [
       {
